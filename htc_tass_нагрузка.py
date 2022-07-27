fbf5222c5b59efc6259b486683573ff04b5af0df
akolzin <59665736+akolzin@users.noreply.github.com>
2022-07-27 18:00:28 +0400

    +

--- htc_tass_нагрузка.py
+++ htc_tass_нагрузка.py
@@ -1,24 +1,12 @@
 import fileinput
 import sys
 
+# Вариант 1 - удаление пустых строк в файле
 # for line in fileinput.FileInput("C:\\Users\\akolzin\\Downloads\\Лист1 — копия.csv",inplace=1):
 #     if line.rstrip():
 #         print(line)
 
-# string = open('C:\\Users\\akolzin\\Downloads\\Лист1 — копия.csv').readlines()
-#
-# for i in string:
-#     if not i.isspace():
-#         print(i.replace('\n', ''))
-
-# with open("C:\\Users\\akolzin\\Downloads\\Лист1 — копия.csv","r") as f:
-#     for line in f:
-#         if not line.isspace():
-#             sys.stdout.write(line)
-
-# with open("C:\\Users\\akolzin\\Downloads\\Лист1 — копия1.csv", "r") as f:
-#     print("".join(line for line in f if not line.isspace()))
-
+# Вариант 2 - удаление пустых строк в файле
 with open('C:\\Users\\akolzin\\Downloads\\Лист1 — копия1.csv', 'r') as inf:
     for line in inf:
         if line.strip():
